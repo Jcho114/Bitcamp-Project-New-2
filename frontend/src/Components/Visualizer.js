346e861abe5de5bdf2f0ba721956daafd0606603
AdityaShelke29 <adityashelke2014@gmail.com>
2023-04-08 23:46:48 -0400

    added base version of Visualizer and Playground

--- frontend/src/Components/Visualizer.js
+++ frontend/src/Components/Visualizer.js
@@ -1,26 +1,59 @@
-<<<<<<< Updated upstream
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { useLeaflet, MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import 'leaflet/dist/leaflet.css'
 
-const Slider = () => {
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
 
-}
 
 const Visualizer = () => {
-    const [year, setYear] = useState(2011);
+    const position = [51, -0.09];
+    
+    const data = [
+        {
+            zipcode: 90000,
+            latitude: 52.605, 
+            longitude: -0.09, 
+            message: 'This is the first message.'
+        }, 
+        {
+            zipcode: 90001,
+            latitude: 52.505, 
+            longitude: -0.09, 
+            message: 'This is the second message.'
+        }
+    ]
 
+   return (
+
+       <div>
+        
+        <MapContainer center = {position} zoom={13} scrollWheelZoom={false} style = {{height: 600, width : "50vw"}}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+               />
+
+
+            {data.map((zip) => (
+                <Marker position={[zip.latitude, zip.longitude]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} >
+                    <Popup>
+                        {zip.message}
+                    </Popup>
+                </Marker>
+            ))}
+           
+           </MapContainer>
+        
+        </div>
+
+
+
+
+
+
+   );
 }
+
 
 export default Visualizer;
-=======
-import React from 'react'
-
-const Visualizer = () => {
-  return (
-    <div>
-        Visualizer
-    </div>
-  )
-}
-
-export default Visualizer
->>>>>>> Stashed changes
--- frontend/src/Playground.js
+++ frontend/src/Playground.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useLeaflet, MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import 'leaflet/dist/leaflet.css'
+import Visualizer from './Components/Visualizer'
+import './Playground.css'
 
 const PlayGround = () => {
     const position = [36.77, 119.4];
 
 	return (
 
-		<div>
+		<div className = "playground-main">
 			<Visualizer/>
 		</div>
 
