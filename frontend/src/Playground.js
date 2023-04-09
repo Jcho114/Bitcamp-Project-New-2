29e926d6bbec7cd7812907dd41dbea1aa017387b
AdityaShelke29 <adityashelke2014@gmail.com>
2023-04-09 04:04:49 -0400

    commit extra

--- frontend/src/Playground.js
+++ frontend/src/Playground.js
@@ -1,21 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { useLeaflet, MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import 'leaflet/dist/leaflet.css'
+import React, { useState } from "react";
+import Slider from "@mui/material/Slider";
+
 import Visualizer from './Components/Visualizer'
 import './Playground.css'
 
-const PlayGround = () => {
-    const position = [119.4179, 119.4179];
-
+const Playground = () => {
+	const [value, setValue] = useState();
+	function handleRangeSlider(value) {
+	   setValue(value);
+	}
 	return (
+	   <div className = "playground-main">
+ 
+		 <p className = "title-segment"> Predicted / Historical California Housing Prices for {value}. </p>
+ 
+		  <Visualizer value = {value} />
 
-		<div className = "playground-main">
-			<Visualizer/>
-		</div>
+		<div className = "buffer"></div>
 
+		<div style = {{ width: "600px" }}>
+            <Slider
+               aria-label = "discrete slider."
+               defaultValue = {2020}
+               getAriaValueText = {handleRangeSlider}
+               valueLabelDisplay = "auto"
+               step = {1}
+               marks
+               min = {2011}
+               max = {2031}
+            />
+         </div>
 
-
-    );
-}
-
-export default PlayGround;
+      </div>
+   );
+};
+export default Playground;
